41de36e1a2b8431b509cef2b64ea22fcaedad6a0
Fram3z <90865381+Fram3z@users.noreply.github.com>
2021-09-21 12:47:12 -0600

    Create countdown.ino

---
+++ countdown.ino
@@ -0,0 +1,15 @@
+void setup() {
+    Serial.begin(9600);
+    int x = 1;
+  for (int i = 0; i <= 15; i = i + x){
+    Serial.println("Countdown:"); 
+    Serial.println(i); 
+    delay(00);
+  
+  }
+}
+
+
+void loop() {
+
+}
